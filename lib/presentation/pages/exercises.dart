f52d7806e2310423e4d6c4dba3423bacd73cf504
Tekletsadik Abebayehu <it.tekletsadik.abebayehutirfe@gmail.com>
2021-08-27 20:23:59 +0300

    Exercise recommendation page UI done

--- lib/presentation/pages/exercises.dart
+++ lib/presentation/pages/exercises.dart
@@ -147,7 +147,281 @@ class ExercisePage extends StatelessWidget {
           ),
         ),
       );
-    } else
-    {
-      return Container()
+    } else {
+      return SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 25, 5, 5),
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    "Choose workout plan",
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        elevation: 3.0,
+                        color: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          side: new BorderSide(
+                              color: Colors.blueGrey, width: 1.0),
+                        ),
+                        child: Column(
+                          children: [
+                            Image.network(
+                              'https://images.unsplash.com/photo-1603287681836-b174ce5074c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=gorilla-freak-wt5jg8_WrJg-unsplash.jpg',
+                            ),
+                            Theme(
+                              data: Theme.of(context).copyWith(
+                                unselectedWidgetColor: Colors.orange,
+                              ),
+                              child: ExpansionTile(
+                                backgroundColor: Colors.transparent,
+                                title: Column(
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        const Text(
+                                          'Weight loss',
+                                          style: TextStyle(
+                                            fontSize: 17,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        Spacer(),
+                                        Text(
+                                          'Levels: 3',
+                                          textAlign: TextAlign.right,
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        Spacer(),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                children: <Widget>[
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(15, 8, 0, 5),
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Column(
+                                        children: [
+                                          const Text(
+                                            "Beignner",
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pullup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          const Text(
+                                            "Intermediate",
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          const Text(
+                                            "Advanced",
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "- Pushup",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white54,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ElevatedButton(
+                                      style: startbuttonStyle,
+                                      onPressed: () {},
+                                      child: const Text("Start"),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
     }
+  }
+}
