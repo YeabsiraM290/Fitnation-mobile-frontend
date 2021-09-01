2f261a101d4737511cad43b78bff1d2b53c8377e
Natnael123456 <it.natnael.zerihun@gmail.com>
2021-08-31 20:37:31 -0700

    profile page bloc intergration

--- lib/presentation/main_page/main_page.dart
+++ lib/presentation/main_page/main_page.dart
@@ -1,7 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:fitnation_frontend/application/auth/profile-form/profile-form_bloc.dart';
+import 'package:fitnation_frontend/application/auth/auth_bloc.dart';
+import 'package:fitnation_frontend/application/profile/profile_form/profile_form_bloc.dart';
+import 'package:fitnation_frontend/application/profile/profile_watcher/profile_watcher_bloc.dart';
 import 'package:fitnation_frontend/presentation/Icons/bottom_nav_icons_icons.dart';
 import 'package:fitnation_frontend/presentation/pages/diet_page.dart';
 import 'package:fitnation_frontend/presentation/pages/exercise_page.dart';
@@ -9,6 +13,7 @@ import 'package:fitnation_frontend/presentation/pages/profile.dart';
 import 'package:fitnation_frontend/presentation/pages/status.dart';
 import 'package:fitnation_frontend/injection.dart';
 import 'package:fitnation_frontend/presentation/pages/timeline_page.dart';
+import 'package:fitnation_frontend/presentation/routes/router.gr.dart';
 
 //body colors
 Color gradientlighttop = HexColor("#1c2732");
@@ -79,9 +84,56 @@ class MainPageBlueprint extends StatelessWidget {
               DietPage(),
               TimeLinePage(),
               StatusPage(),
-              BlocProvider(
-                create: (context) => getIt<ProfileFormBloc>(),
-                child: ProfilePage(),
+              MultiBlocProvider(
+                providers: [
+                  BlocProvider<ProfileWatcherBloc>(
+                    create: (context) => getIt<ProfileWatcherBloc>()
+                      ..add(const ProfileWatcherEvent.watchStarted()),
+                  ),
+                  BlocProvider<ProfileFormBloc>(
+                    create: (context) => getIt<ProfileFormBloc>(),
+                  ),
+                ],
+                child: MultiBlocListener(
+                    listeners: [
+                      BlocListener<AuthBloc, AuthState>(
+                        listener: (context, state) {
+                          state.maybeMap(
+                            unauthenticated: (_) =>
+                                ExtendedNavigator.of(context)
+                                    .replace(Routes.loginFormContainer),
+                            orElse: () {},
+                          );
+                        },
+                      ),
+                    ],
+                    child: BlocBuilder<ProfileWatcherBloc, ProfileWatcherState>(
+                      builder: (context, state) {
+                        return state.map(
+                          initial: (_) => Container(),
+                          loadInProgress: (_) => const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                          loadSuccess: (state) {
+                            final user = state.user;
+                            return ProfilePage(
+                              user: user,
+                            );
+                          },
+                          loadFailure: (state) {
+                            return Center(
+                              child: Text(
+                                "User Not Found !",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    )),
               ),
             ],
           ),
