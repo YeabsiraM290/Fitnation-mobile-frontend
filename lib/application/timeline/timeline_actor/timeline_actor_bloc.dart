841fb1fd22ed8c7cbdbba067c844c8d6fe44347d
ermiasmulu19 <ermiasmulu19@gmail.com>
2021-09-07 05:27:37 +0300

    adding timeline on the application layer

---
+++ lib/application/timeline/timeline_actor/timeline_actor_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:meta/meta.dart';
+import 'package:temp_builder/domain/timeline/timeline_failure.dart';
+import 'package:temp_builder/domain/timeline/timeline_repository.dart';
+
+part 'timeline_actor_event.dart';
+part 'timeline_actor_state.dart';
+part 'timeline_actor_bloc.freezed.dart';
+
+@injectable
+class TimelineActorBloc extends Bloc<TimelineActorEvent, TimelineActorState> {
+  final TimelineRepository _timelineRepository;
+
+  TimelineActorBloc(this._timelineRepository)
+      : super(const TimelineActorState.initial());
+
+  @override
+  Stream<TimelineActorState> mapEventToState(
+    TimelineActorEvent event,
+  ) async* {
+    yield const TimelineActorState.actionInProgress();
+    final possibleFailure = await _timelineRepository.resetHistory();
+    yield possibleFailure.fold(
+      (f) => TimelineActorState.resetFailure(f),
+      (_) => const TimelineActorState.resetSuccess(),
+    );
+  }
+}
