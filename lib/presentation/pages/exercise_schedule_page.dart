7dcdd70da3256673050adb0d8cb61f1b9b594f75
Tekletsadik Abebayehu <it.tekletsadik.abebayehutirfe@gmail.com>
2021-09-08 23:32:58 +0300

    finished the exercise schedule

---
+++ lib/presentation/pages/exercise_schedule_page.dart
@@ -0,0 +1,138 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:temp_builder/application/exercise/exercise_actor/exercise_actor_bloc.dart';
+import 'package:temp_builder/application/exercise/exercise_watcher/exercise_watcher_bloc.dart';
+import 'package:temp_builder/domain/exercise/exercise_failure.dart';
+import 'package:temp_builder/domain/exercise/exercise_plan.dart';
+import 'package:temp_builder/domain/exercise/schedule.dart';
+import 'package:temp_builder/domain/exercise/value_objects.dart';
+import 'package:temp_builder/presentation/routes/router.gr.dart';
+import 'package:temp_builder/presentation/user_pages/exercise/widget/day_view.dart';
+import 'package:temp_builder/presentation/user_pages/main_page/main_page.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+Color startbuttoncolor = HexColor("#3aaa28");
+
+final startbuttonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(startbuttoncolor),
+    textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
+      fontSize: 15,
+    )));
+
+YoutubePlayerController controller(String url) {
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: YoutubePlayer.convertUrlToId(url),
+    flags: const YoutubePlayerFlags(
+      controlsVisibleAtStart: true,
+      autoPlay: false,
+    ),
+  );
+
+  return _controller;
+}
+
+class UserExercisePage extends StatelessWidget {
+  final Schedule schedule;
+
+  const UserExercisePage({
+    Key key,
+    this.schedule,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var screen_size = MediaQuery.of(context).size;
+
+    return BlocConsumer<ExerciseActorBloc, ExerciseActorState>(
+      listener: (context, state) {
+        state.maybeMap(
+          quiteFailure: (state) {
+            FlushbarHelper.createError(
+              duration: const Duration(seconds: 6),
+              message: state.exerciseFailure.map(
+                  serverError: (_) => 'Server error',
+                  exerciseNotFound: (_) => ' ',
+                  exerciseExists: (_) => ' ',
+                  exerciseNotSelected: (_) => 'You no plan to quite'),
+            ).show(context);
+          },
+          quiteSuccess: (state) {
+            ExtendedNavigator.of(context).replace(Routes.mainPageBlueprint);
+          },
+          orElse: () {},
+        );
+      },
+      builder: (context, state) {
+        return DefaultTabController(
+          initialIndex: 0,
+          length: 7,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: GestureDetector(
+                onTap: () {
+                  context
+                      .read<ExerciseActorBloc>()
+                      .add(ExerciseActorEvent.quited(Name('quite')));
+                },
+                child: const Icon(
+                  Icons.delete,
+                  size: 20,
+                  color: Colors.redAccent,
+                ),
+              ),
+              bottom: const TabBar(
+                indicatorColor: Colors.green,
+                labelColor: Colors.green,
+                unselectedLabelColor: Colors.grey,
+                tabs: <Widget>[
+                  Tab(
+                    text: "M",
+                  ),
+                  Tab(
+                    text: "T",
+                  ),
+                  Tab(
+                    text: "W",
+                  ),
+                  Tab(
+                    text: "T",
+                  ),
+                  Tab(
+                    text: "F",
+                  ),
+                  Tab(
+                    text: "S",
+                  ),
+                  Tab(
+                    text: "S",
+                  ),
+                ],
+              ),
+            ),
+            body: Container(
+              height: MediaQuery.of(context).size.height,
+              child: TabBarView(
+                children: [
+                  DayView(workouts: schedule.monday),
+                  DayView(workouts: schedule.tuesday),
+                  DayView(workouts: schedule.wednesday),
+                  DayView(workouts: schedule.thursday),
+                  DayView(workouts: schedule.friday),
+                  DayView(workouts: schedule.saturday),
+                  DayView(workouts: schedule.sunday),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
