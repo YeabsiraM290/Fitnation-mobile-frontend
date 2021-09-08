60875ab3ce24722af7695eb3364bc308922ae107
ermiasmulu19 <ermiasmulu19@gmail.com>
2021-09-08 23:50:20 +0300

    worouts misc in the presentation layer

---
+++ lib/presentation/workouts_misc/build_context_x.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:kt_dart/collection.dart';
+
+import 'package:provider/provider.dart';
+import 'package:temp_builder/presentation/user_pages/timeline/workouts_misc/workouts_presentation_classes.dart';
+
+extension AllWorkoutsX on BuildContext {
+  KtList<WorkoutsPrimitive> get formTodos =>
+      Provider.of<AllWorkouts>(this, listen: false).value;
+  set allLevls(KtList<WorkoutsPrimitive> value) =>
+      Provider.of<AllWorkouts>(this, listen: false).value = value;
+}
---
+++ lib/presentation/workouts_misc/workouts_presentation_classes.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/kt.dart';
+
+import 'package:temp_builder/domain/exercise/value_objects.dart';
+import 'package:temp_builder/domain/exercise/workout.dart';
+
+part 'workouts_presentation_classes.freezed.dart';
+
+class AllWorkouts extends ValueNotifier<KtList<WorkoutsPrimitive>> {
+  AllWorkouts() : super(emptyList<WorkoutsPrimitive>());
+}
+
+@freezed
+abstract class WorkoutsPrimitive implements _$WorkoutsPrimitive {
+  const WorkoutsPrimitive._();
+
+  const factory WorkoutsPrimitive({
+    @required String name,
+    @required int rep,
+    @required String tutorial,
+  }) = _WorkoutsPrimitive;
+
+  factory WorkoutsPrimitive.empty() => WorkoutsPrimitive(
+        name: '',
+        rep: 0,
+        tutorial: '',
+      );
+
+  factory WorkoutsPrimitive.fromDomain(Workout workout) {
+    return WorkoutsPrimitive(
+      name: workout.name.getOrCrash().toString(),
+      rep: workout.rep.getOrCrash().toInt(),
+      tutorial: workout.tutorial.getOrCrash().toString(),
+    );
+  }
+
+  Workout toDomain() {
+    return Workout(
+      name: Name(name),
+      rep: Repetation(rep),
+      tutorial: Url(tutorial),
+    );
+  }
+}
