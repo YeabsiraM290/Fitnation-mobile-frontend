5f114f845169d85c3547f078bda056533ea8d315
dawit yitayal <it.dawit.yitayal@gmail.com>
2021-09-07 19:11:23 -0700

    presentation diet misc

---
+++ lib/presentation/pages/diet_misc/build_context_x.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:temp_builder/presentation/user_pages/diet/diet_misc/schedule_presentation_classes.dart';
+import 'package:provider/provider.dart';
+
+extension AllScheduleX on BuildContext {
+  KtList<SchedulePrimitive> get formTodos =>
+      Provider.of<AllSchedule>(this, listen: false).value;
+  set allLevls(KtList<SchedulePrimitive> value) =>
+      Provider.of<AllSchedule>(this, listen: false).value = value;
+}
